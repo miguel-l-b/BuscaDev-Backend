2cbd849c026ce26d26b0f6ddbb71d1f876b37883
Miguel-Coruj <miguelcoruj@gmail.com>
2020-01-16 22:29:54 -0300

    'Back-end'

---
+++ src/controllers/SearchController.js
@@ -0,0 +1,29 @@
+const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
+
+module.exports = {
+    async index(req, res) {
+        const { latitude, longitude, techs, distancia } = req.query
+
+        const techsArray = parseStringAsArray(techs)
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude]
+                    },
+                    $maxDistance: distancia,
+                },           
+            },
+        })
+
+        console.log(techsArray)
+
+        return res.json({ devs })
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const routes = require('./routes')
+
+const Port = 8080
+
+const app = express()
+
+mongoose.connect('mongodb+srv://root:123@cluster0-2tzjk.mongodb.net/test?retryWrites=true&w=majority',{ 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex :  true
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+app.listen(Port)
+console.log('Started | Port:'+Port)
