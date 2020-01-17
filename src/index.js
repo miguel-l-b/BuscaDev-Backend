e1c288ed3ceb18973cc929b56eaed4ea17a3fb81
Miguel-Coruj <miguelcoruj@gmail.com>
2020-01-17 18:33:21 -0300

    'Back-end'

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const routes = require('./routes')
-const img = require('./img')
 
 const Port = 8080
 
@@ -17,7 +16,7 @@ mongoose.connect('mongodb+srv://root:123@cluster0-2tzjk.mongodb.net/test?retryWr
 app.use(cors())
 app.use(express.json())
 app.use(routes)
-app.use('/img', img)
+app.use('/img', express.static('img'))
 app.get('*', (req, res) => {
     res.send('<h1>Api DevBusca</h1> <style>body{ display: flex;flex-direction: row;align-items: center; text-align: center; } h1 { text-size: 25px; }</style>')
 }) 
