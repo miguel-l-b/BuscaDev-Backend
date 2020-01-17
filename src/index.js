297eeedaf3cd98a9d39e82a56e90dd0281f6a9ea
Miguel-Coruj <miguelcoruj@gmail.com>
2020-01-17 18:37:12 -0300

    'Back-end'

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use('/img', express.static('img'))
-app.get('*', (req, res) => {
-    res.send('<h1>Api DevBusca</h1> <style>body{ display: flex;flex-direction: row;align-items: center; text-align: center; } h1 { text-size: 25px; }</style>')
+app.get('/', (req, res) => {
+    res.send('<h1>Api DevBusca</h1> <style>body{ display: flex;flex-direction: row;align-items: center; text-align: center; } h1 { text-align: center; text-size: 25px; }</style>')
 }) 
 
 app.listen(process.env.PORT || Port)
