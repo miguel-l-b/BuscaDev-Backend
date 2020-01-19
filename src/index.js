3eb830405e188af82d21cdf5e3cf4038456645bd
Miguel Lopes <miguelcoruj@gmail.com>
2020-01-19 15:31:16 +0000

    Back-end

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const http = require('http')
+
 const routes = require('./routes')
+const { setupWebsocket } = require('./websocket')
 
 const Port = 8080
 
 const app = express()
+const server = http.Server(app)
+
+
+setupWebsocket(server)
 
 mongoose.connect('mongodb+srv://root:123@cluster0-2tzjk.mongodb.net/test?retryWrites=true&w=majority',{ 
     useNewUrlParser: true,
@@ -18,5 +25,5 @@ app.use(express.json())
 app.use(routes)
 app.use(express.static('img'))
 
-app.listen(process.env.PORT || Port)
+Server.listen(process.env.PORT || Port)
 console.log('Started | Port:'+process.env.PORT || Port)
